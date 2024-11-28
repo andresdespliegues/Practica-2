ea3a27846bc2ad58e29440a8301bb4f5090684a7
andresdespliegues <andresdespliegues@gmail.com>
2024-11-28 10:09:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -200,18 +200,16 @@ resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.bucket.id
 
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid = "AllowPublicRead",
-        Effect = "Allow",
-        Principal = "*",
-        Action   = "s3:GetObject",
-        Resource = "${aws_s3_bucket.bucket.arn}/*"
-      }
+   "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:PutBucketPolicy",
+      "Resource": "arn:aws:s3:::aopdebianbucket1"
+    }
     ]
   })
-}
+  }
 
 resource "aws_s3_bucket_object" "index_html" {
   bucket = aws_s3_bucket.bucket.id
